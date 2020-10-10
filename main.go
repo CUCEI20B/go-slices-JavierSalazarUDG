c8785e537e8abc15f8588df37cdb4c0b0581e39f
Javier salazar <jsalazar@lumston.com>
2020-10-10 12:35:06 -0500

    change int to float

--- main.go
+++ main.go
@@ -3,19 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	var s []int
+	var s []float32
 	var length int
 	fmt.Scan(&length)
-	s = append(s, length)
+	s = append(s, float32(length))
 	for i := 0; i < length; i++ {
-		var value int
+		var value float32
 		fmt.Scan(&value)
 		s = append(s, value)
 	}
 	sum := s[1 : length+1]
-	var res int
+	var res float32
 	for _, v := range sum {
 		res = res + v
 	}
-	print(res)
+	fmt.Printf("%f\n", res)
 }
