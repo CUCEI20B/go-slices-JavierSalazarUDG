e2315e1b98cd620b2b01a8e0e97e390b4cd9066b
Javier salazar <jsalazar@lumston.com>
2020-10-10 12:27:33 -0500

    finish homework

--- main.go
+++ main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var s []int
+	var length int
+	fmt.Scan(&length)
+	s = append(s, length)
+	for i := 0; i < length; i++ {
+		var value int
+		fmt.Scan(&value)
+		s = append(s, value)
+	}
+	sum := s[1 : length+1]
+	var res int
+	for _, v := range sum {
+		res = res + v
+	}
+	print(res)
 }
